fix the masking error

main.py
@@ -1,4 +1,5 @@
-import tiktoken
+from loguru import logger
+from tokenizers import Tokenizer
 import torch
 from torch.utils.data import DataLoader
 from torch import nn
@@ -10,7 +11,7 @@ from src.train import do_epoch
 
 def main():
     device = torch.device("cpu" if torch.cuda.is_available() else "cpu")
-    tokenizer = tiktoken.get_encoding("gpt2")
+    tokenizer: Tokenizer = Tokenizer.from_pretrained("openai-community/gpt2")
 
     # Load datasets
     train_ds = load_dataset("bentrevett/multi30k", split="train").take(10)  # type: ignore
@@ -25,7 +26,7 @@ def main():
     val_loader = DataLoader(val_dataset, batch_size=2, shuffle=False, collate_fn=collate_fn)
 
     # Model
-    vocab_size = tokenizer.n_vocab
+    vocab_size = tokenizer.get_vocab_size()
     model = Transformer(
         enc_vocab_size=vocab_size,
         dec_vocab_size=vocab_size,
@@ -53,8 +54,8 @@ def main():
         val_loss = do_epoch(
             model=model, criterion=criterion, loader=val_loader, tokenizer=tokenizer, epoch=epoch
         )
-        print(train_loss)
-        print(
+
+        logger.info(
             f"Epoch {epoch + 1} / {epochs}: Train loss [{train_loss['loss']:.4f}] | Val loss {val_loss['loss']:.4f}"
         )
 

src/data.py
@@ -1,8 +1,8 @@
 import torch
-
+from tokenizers import Tokenizer, Encoding
 
 class Multi30kDataset(torch.utils.data.Dataset):
-    def __init__(self, hf_dataset, tokenizer, max_len=50):
+    def __init__(self, hf_dataset, tokenizer: Tokenizer, max_len=50):
         self.dataset = hf_dataset
         self.tokenizer = tokenizer
         self.max_len = max_len
@@ -13,15 +13,15 @@ class Multi30kDataset(torch.utils.data.Dataset):
     def __getitem__(self, idx):
         src_text = self.dataset[idx]["en"]
         tgt_text = self.dataset[idx]["de"]
-        src_tokens = self.tokenizer.encode(src_text)[: self.max_len]
-        tgt_tokens = self.tokenizer.encode(tgt_text)[: self.max_len]
+        src_tokens: Encoding = self.tokenizer.encode(src_text).ids[: self.max_len]
+        tgt_tokens: Encoding = self.tokenizer.encode(tgt_text).ids[: self.max_len]
         return {
             "src": torch.tensor(src_tokens, dtype=torch.long),
             "tgt": torch.tensor(tgt_tokens, dtype=torch.long),
         }
 
 
-def collate_fn(batch):
+def collate_fn(batch) -> tuple[torch.Tensor, torch.Tensor]:
     def pad_sequence(seqs, pad_id: int = 0):
         max_len = max(seq.size(0) for seq in seqs)
         return torch.stack(

src/train.py
@@ -1,8 +1,8 @@
 from loguru import logger
 from torch.utils.data import DataLoader
 from torch import nn
+from tokenizers import Tokenizer
 import torch
-import tiktoken
 from sacrebleu.metrics import BLEU, CHRF
 from src.utils import averager, create_masks
 
@@ -12,7 +12,7 @@ def do_epoch(
     model: nn.Module,
     loader: DataLoader,
     criterion: nn.Module,
-    tokenizer: tiktoken.Encoding,
+    tokenizer: Tokenizer,
     optimizer=None,
 ):
     """Run a single epoch, either in training or evaluation mode, if `optimizer` is None."""
@@ -24,26 +24,25 @@ def do_epoch(
     chrf = CHRF()
 
     for src, tgt in loader:
-        print("src", src.shape, "tgt", tgt.shape)
+ 
         src, tgt = src.to(device), tgt.to(device)
-        src_mask, tgt_mask = create_masks(src, tgt)
         tgt_input = tgt[:, :-1]
         tgt_output = tgt[:, 1:]
+        src_mask, tgt_mask = create_masks(src, tgt_input)
         with torch.set_grad_enabled(optimizer is not None):
             prediction = model(
                 src,
                 tgt_input,
-                #    src_mask=src_mask, tgt_mask=tgt_mask,
+                   src_mask=src_mask, tgt_mask=tgt_mask,
             )
 
             loss = criterion(prediction.reshape(-1, prediction.size(-1)), tgt_output.reshape(-1))
             pred_tokens = prediction.argmax(dim=-1)
-            decoded_references = [tokenizer.decode(ref.tolist()).split() for ref in tgt_output]
+            decoded_references: list[str] = [tokenizer.decode(ref.tolist()).split() for ref in tgt_output]
             decoded_hypotheses = [tokenizer.decode(hyp.tolist()).split() for hyp in pred_tokens]
-            references = [" ".join(ref) for ref in decoded_references]
+            references = [ref for ref in decoded_references]
             hypotheses = [" ".join(hyp) for hyp in decoded_hypotheses]
 
-            print("references", references)
             score = bleu.corpus_score(hypotheses=hypotheses, references=references)
             chrf_score = chrf.corpus_score(hypotheses=hypotheses, references=references)
 
@@ -59,6 +58,6 @@ def do_epoch(
     logger.info(
         f"Epoch {epoch:03d} {label: <5} summary "
         f"loss: {metrics['loss']:.3f}, "
-        f"acc.: {metrics['bleu']:6.2%}"
+        f"BLEU.: {metrics['bleu']:6.2%}"
     )
     return metrics